fb71104507c5853a4a1359d208ded83d4a41773b
oburt <orionjburt@gmail.com>
2012-11-22 07:28:55 +0000

    first night. index and builders rough.

--- www/burt-engineering/builders.php
+++ www/burt-engineering/builders.php
@@ -9,7 +9,69 @@
 				</div>
 			</div>
 			<div class="row" style="margin-top:30px;">
-
+				<div class="span3 be-sidebar">
+					<ul class="nav" data-spy="affix" data-offset-top="360">
+						<li><a href="#seismic-strengthening">Seismic Strengthening</a></li>
+						<li><a href="#timber-framing">Timber Framing</a></li>
+						<li><a href="#historic-preservation">Historic Preservation</a><li>
+					</ul>
+				</div>
+				<div class="span9">
+					<section id="seismic-strengthening" class="row">
+						<div class="span5">
+							<h2>Seismic Strengthening</h2>
+							<h3>Starbucks, Renton, WA</h3>
+							<p>
+								This project at Exit 7 in Renton was a new reinforced concrete masonry unit retail building for a Starbucks anchor tenant. The building is approximately 2,900 sq ft, and was designed to fit neatly into an odd shaped lot. It features a drive up window, and a tower that stands proud of the roof plane for displaying the classic Starbucks logo.
+							</p>
+							<p>
+								The site was challenging as it was to be situated on structurally poor soil. The structure had to be supported on a system of auger cast concrete piles and cast in place structural concrete grade beams. The roof was of open web steel joist construction, and canopies cover the retail entry.
+							</p>
+							<p>
+								An additional challenge was that this project had to be designed on a Fast Track due to a lease agreement. We completed the design work on the project more than 1 week ahead of schedule!
+							</p>
+						</div>
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/starbucks-renton.jpg" />
+							</a>
+						</div>
+					</section>
+					<section id="timber-framing" class="row">
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/timber-framing.jpg" />
+							</a>
+						</div>
+						<div class="span5">
+							<h2>Timber Framing</h2>
+							<h3>Swauk Ranch</h3>
+							<p>
+								This is a new structure that is comprised of 5 buildings attached in a row. This building is built green. The trusses shown are site built from heavy timber, 50% of which was milled on site and the rest was from old growth lumber reclaimed from a 100 year old warehouse in Seattle.
+							</p>
+							<p>
+								The trusses span 40 feet, supporting an 80 psf roof snow load. The trusses feature a clerestory, letting natural light in. The building includes a ranch maintenance shop, office space, and a small residence.
+							</p>
+							<p>
+								The project also includes a wind turbine generator to reduce demand on the grid.
+							</p>
+						</div>
+					</section>
+					<section id="historic-preservation" class="row">
+						<div class="span5">
+							<h2>Historic Preservation</h2>
+							<h3>Kurzweg Cabin at Snoqualmie Pass</h3>
+							<p>
+								This cabin originally had an A framed roof. An old growth douglas fir tree fell across the entry. In rebuilding this historic site, we had to match the historic requirements of the original cabin as much as possible. We incorporated a space frame truss system to accommodate a 262 psf ground snow load while maintaining the openness of the entry of the cabin.
+							</p>
+						</div>
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/cabin-older-as-built.jpg" />
+							</a>
+						</div>
+					</section>
+				</div>
 			</div>
 		</div>
 	</div>
