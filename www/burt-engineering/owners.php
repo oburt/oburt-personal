db80b9c7a5e6b6033d6edde04e56f543653e09c2
oburt <orionjburt@gmail.com>
2012-11-23 21:21:45 +0000

    owners first draft

--- www/burt-engineering/owners.php
+++ www/burt-engineering/owners.php
@@ -1,40 +1,97 @@
-For Property Owners
-Services for existing structures
-    
-Seismic Evaluation and Analysis: Our analysis focuses on specific performance objectives, suited to the project, with affordable techniques that are rooted in the historic performance of buildings in earthquakes.
+<?php $page = 'owners'; ?>
+<?php require_once('header.php'); ?>
 
-Seismic Hazard Reduction Programs: We can aid in the development of and implementation of appropriate mitigation strategies for large inventories of hazardous buildings.
-Seismic Risk Assessment and PMLs: PML studies are based on appropriate inspection, historic performance, engineering intuition, performance levels and acceptable levels of safety and presented with an expert opinion about how to reduce the expected loss.
-Seismic Strengthening: Our seismic strengthening services are designed to be executed with the least disruption and at minimum cost.
-Pre-lease/Pre-purchase Investigations: We provide expert opinions on compliance with applicable and current codes and specific, understandable performance information needed for informed choices.
+	<div class="be-content">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<img src="http://placehold.it/1170x200" />
+				</div>
+			</div>
+			<div class="row" style="margin-top:30px;">
+				<div class="span3 be-sidebar">
+					<ul class="nav" data-spy="affix" data-offset-top="360">
+						<li><a href="#seismic-evaluation">Seismic Evaluation and Analysis</a></li>
+						<li><a href="#project-management">Project Management</a></li>
+						<li><a href="#peer-review">Peer Review/Value Engineering</a><li>
+						<li><a href="#pre-investigations">Pre-lease/Pre-purchase Investigations</a><li>
+					</ul>
+				</div>
+				<div class="span9">
+					<section id="seismic-evaluation" class="row">
+						<div class="span5">
+							<h2>Seismic Evaluation and Analysis</h2>
+							<h3>Tatwai House</h3>
+							<p>
+								We modernized and practically doubled the size of this house, using a wood framed seismic resistance system with a mostly new foundation system and engineered lumber to support the 2nd story.
+							</p>
+							<p>
+								The site was underlain with poorly draining soil and had to be de-watered prior to construction.
+							</p>
+							<p>
+								Burt Engineering also provides nonstructural design as needed to achieve specific seismic performance requirement as well as blast resistant design services as an extension of proper earthquake resistant design.
+							</p>
+						</div>
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/tatwai-house.jpg" />
+							</a>
+						</div>
+					</section>
+					<section id="project-management" class="row">
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/sammamish-retaining-structure.jpg" />
+							</a>
+						</div>
+						<div class="span5">
+							<h2>Project Management</h2>
+							<h3>Sammamish River Soil Retaining Structure</h3>
+							<p>
+								A retaining wall at the top of a steep slope environmentally critical area in above the Sammamish river valley in Woodinville. The wall was failing, sliding downward as the top of the slope was subsiding.
+							</p>
+							<p>
+								Working closely with the geotechnical engineers at Nelson Geotechnical Associates, we designed a pressure grouted tie-back system to laterally anchor the wall above the global failure plane. The wall is supported horizontally by a waler and vertically by pipe piles. Equipment access was limited, so the design had to accommodate that challenge.
+							</p>
+							<p>
+								Our project management services are offered to clients in need of assistance with a complex hazard mitigation program.
+							</p>
+						</div>
+					</section>
+					<section id="peer-review" class="row">
+						<div class="span5">
+							<h2>Peer Review/Value Engineering</h2>
+							<h3>Krompholz Residence</h3>
+							<p>
+								Helped make a house accessible for a young handicapped man.
+							</p>
+							<p>
+								Peer review services balance the opinion of the reviewer and the design professionals of record, and are conducted in a manner that informs the owner of the issues in understandable language and the options to quickly remedy the disagreements.
+							</p>
+						</div>
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/krompholz-residence.jpg" />
+							</a>
+						</div>
+					</section>
+					<section id="pre-investigations" class="row">
+						<div class="span4">
+							<a href="#" class="thumbnail">
+								<img src="img/school.jpg" />
+							</a>
+						</div>
+						<div class="span5">
+							<h2>Pre-lease/Pre-purchase Investigations</h2>
+							<h3>School</h3>
+							<p>
+								We provide expert opinions on compliance with applicable and current codes and specific, understandable performance information needed for informed choices.
+							</p>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
 
-Nonstructural Design: Nonstructural design is needed to achieve specific performance goals including operational levels of seismic performance.
-
-Blast Resistant Design: Burt Engineering provides blast resistant design services as an extension of proper earthquake resistant design.
-
-Peer Review/Value Engineering: Peer review services balance the opinion of the reviewer and the design professionals of record, and are conducted in a manner that informs the owner of the issues in understandable language and the options to quickly remedy the disagreements.
-
-Program Management: Our project management services are offered to client in need of assistance with a complex hazard mitigation program.
-
-
---School
-
-
---Sammamish River Soil Retaining Structure
-A retaining wall at the top of a steep slope environmentally critical area in above the Sammamish river valley in Woodinville. The wall was failing, sliding downward as the top of the slope was subsiding.
-
-Working closely with the geotechnical engineers at Nelson Geotechnical Associates, we designed a pressure grouted tie-back system to laterally anchor the wall above the global failure plane. The wall is supported horizontally by a waler and vertically by pipe piles. Equipment access was limited, so the design had to accommodate that challenge.
-
-
---Krompholz Residence
-Making a house accessible for a young handicapped man.
-
-
---Hoffman House
-We added a third floor to this classic home overlooking Lake Washington. The cantilevered overhang made for a unique engineering challenge. We drove all new piers into...
-
-
---Tatwai Ho (House, Hocho?)
-We modernized and practically doubled the size of this house, using a wood framed seismic resistance system with a mostly new foundation system and engineered lumber to support the 2nd story.
-
-The site was underlain with poorly draining soil and had to be de-watered prior to construction.
+<?php require_once('footer.php'); ?>
