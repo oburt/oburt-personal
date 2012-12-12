78d6096d87595a25ac37c7c06bcfd4c82a8a6383
oburt <orionjburt@gmail.com>
2012-12-12 19:42:19 +0000

    index page info boxes use vertical-align and inline-blocks to center vertically and wrap. also, phone and portrait tablet responsive done

--- www/burt-engineering/index.php
+++ www/burt-engineering/index.php
@@ -28,25 +28,25 @@
 			<div class="row">
 				<div class="span8 offset2">
 					<div class="be-index-photo thumbnail">
+						<img src="img/cb_deskside-274x331.jpg" />
 						<p>Mr. Burt holds a Master of Science in Civil Engineering from Washington State University. He is a state licensed Professional Engineer (WA LICENSE #38541) as well as being fully accredited as a Certified Building Plans Examiner by the International Code Council. Burt Engineering is also a proud member of the following organizations: <em>Structural Engineers Association of Washington, Master Builders Of Washington, American Institute Of Architects.</em>
 						</p>
-						<img src="img/cb_deskside-274x331.jpg" />
 					</div>
 				</div>
 			</div>
 			<div class="row be-index-closer">
 				<div class="span8 offset2">
 					<div class="thumbnail">
+						<img src="img/bg-small-260x320.png" />
 						<p>
 							If you are a builder or an architect:</br>look at some recent examples of our <a href="builders.php">consulting work</a>.
-						</p>
-						<p>
+						<br />
+						<br />
 							If you are a property owner:</br>see some of the  <a href="owners.php">properties we've improved this year</a>.
-						</p>
-						<p>
+						<br />
+						<br />
 							If you want to get a quote:</br>call us at <a href="tel:1-206-779-6856">(206) 779-6856</a> or email us at <a href="mailto:cb@burtengineering.com">cb@burtengineering.com</a>.
 						</p>
-						<img src="img/bg-small-260x320.png" />
 					</div>
 				</div>
 			</div>
